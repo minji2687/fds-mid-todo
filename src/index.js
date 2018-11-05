26a77b57d6a141457334ac50253ae551daa5cd62
Jo Minji <mingi2687@gamil.com>
2018-11-05 15:38:29 +0900

    로그인 요청 성공

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import "@babel/polyfill"; // 이 라인을 지우지 말아주세요!
 
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: "https://probable-page.glitch.me/"
+});
+
 const templates={
   loginForm: document.querySelector('#login-form').content
 }
@@ -11,6 +17,22 @@ function drawLoginForm(){
   const fragment = document.importNode(templates.loginForm, true)
   //2. 내용 채우고 , 이벤트 리스너 등록하기
   const loginFormEl = fragment.querySelector('.login-form')
+
+  loginFormEl.addEventListener('submit', async e=>{
+    e.preventDefault()
+    // e: 이벤트 객체
+    // e.target: 이벤트를 실제로 일으킨 요소 객체 (여기서는 loginFormEl)
+    // e.target.elements: 폼 내부에 들어있는 요소 객체를 편하세 가져올 수 있는 특이한 개체
+    //e.target.element.username : name어트리뷰트에 username 이라고 지정되어 있는 input 요소 객체
+    //.value :사용자가 input 태그에 입려갛ㄴ 값
+    const username = e.target.elements.username.value
+    const password = e.target.elements.password.value
+    const res = await api.post('/users/login',{
+      username,
+      password
+    })
+    alert(res.data.token)
+  })
 
   loginFormEl.addEventListener('submit', async e=>{
     alert('로그인 요청 전송')
