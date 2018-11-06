e108ddd4ad6867c54ffe7151d35d08d0cca6ce37
Jo Minji <mingi2687@gamil.com>
2018-11-06 12:53:53 +0900

    loding-indecator

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "@babel/polyfill"; // 이 라인을 지우지 말아주세요!
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://destiny-screwdriver.glitch.me/"
+  baseURL: "https://abstracted-debt.glitch.me/"
 });
 
 // Axios Interceptor - 그때그때 다른 설정 사용하기
@@ -46,42 +46,47 @@ function drawLoginForm() {
       password
     });
     localStorage.setItem("token", res.data.token);
-    drawTodoList()
+    drawTodoList();
   });
 
   // 3. 문서 내부에 삽입하기
-  rootEl.textContent=''
+  rootEl.textContent = "";
   rootEl.appendChild(fragment);
 }
 
 async function drawTodoList() {
- const res = await api.get('/todos')
- const list = res.data
+  const res = await api.get("/todos");
+  const list = res.data;
   // 1. 템플릿 복사하기
   const fragment = document.importNode(templates.todoList, true);
 
   // 2. 내용 채우고 이벤트 리스너 등록하기
   const todoListEl = fragment.querySelector(".todo-list");
-  const todoFormEl = fragment.querySelector('.todo-form')
-  const logoutEl = fragment.querySelector('.logout')
+  const todoFormEl = fragment.querySelector(".todo-form");
+  const logoutEl = fragment.querySelector(".logout");
 
-  logoutEl.addEventListener('click',e=>{
-    //1.토큰삭제
-    localStorage.removeItem('token')
-    //2.로그인 폼 보여주기
-    drawLoginForm()
-  })
-  todoFormEl.addEventListener('submit',async e=>{
-    e.preventDefault()
-    const body = e.target.elements.body.value
-    const res = await api.post('/todos',{
+  logoutEl.addEventListener("click", e => {
+    // 로그아웃 절차
+    // 1. 토큰 삭제
+    localStorage.removeItem("token");
+    // 2. 로그인 폼 보여주기
+    drawLoginForm();
+  });
+
+  todoFormEl.addEventListener("submit", async e => {
+    document.body.classList.add('loading')
+    // 주석을 풀면 비관적 업데이트로 변화
+    e.preventDefault();
+    const body = e.target.elements.body.value;
+    const res = await api.post("/todos", {
       body,
-      complete:false
-    })
-    if(res.status === 201){
-      drawTodoList();
-    }
-  })
+      complete: false
+    });
+
+     await drawTodoList();
+
+    document.body.classList.remove('loading')
+  });
 
   list.forEach(todoItem => {
     // 1. 템플릿 복사하기
@@ -89,37 +94,44 @@ async function drawTodoList() {
 
     // 2. 내용 채우고 이벤트 리스너 등록하기
     const bodyEl = fragment.querySelector(".body");
+    const deleteEl = fragment.querySelector(".delete");
+    const completeEl = fragment.querySelector(".complete");
+
+    if (todoItem.complete) {
+      completeEl.setAttribute("checked", "");
+    }
 
     bodyEl.textContent = todoItem.body;
 
-    //삭제버튼 만들기
-    const deleteButtonEl = fragment.querySelector(".delete-button")
-    deleteButtonEl.addEventListener('click', async e => {
-      await api.delete('/todos/'+todoItem.id)//데이터 삭제요청 끝
+    completeEl.addEventListener("click", async e => {
+      // e.preventDefault();
 
-       drawTodoList()//리스트 다시 그려주기
+      await api.patch("/todos/" + todoItem.id, {
+        complete: !todoItem.complete
+      });
+      drawTodoList();
+    });
 
-    })//삭제 이벤트 리스너 끝
-    todoListEl.appendChild(deleteButtonEl);
-
+    deleteEl.addEventListener("click", async e => {
+      // 삭제 요청 보내기
+      await api.delete("/todos/" + todoItem.id);
+      // 성공 시 할 일 목록 다시 그리기
+      drawTodoList();
+    });
 
     // 3. 문서 내부에 삽입하기
     todoListEl.appendChild(fragment);
-
-
-
   });
 
   // 3. 문서 내부에 삽입하기
-  rootEl.textContent = ''
+  rootEl.textContent = "";
   rootEl.appendChild(fragment);
 }
-//만약 로그인을 한 상태라면 바로 할 일 목록을 보여주고
-if(localStorage.getItem('token')){
-  drawTodoList()
-}else{
-  drawLoginForm()
+
+// 만약 로그인을 한 상태라면 바로 할 일 목록을 보여주고
+if (localStorage.getItem("token")) {
+  drawTodoList();
+} else {
   // 아니라면 로그인 폼을 보여준다.
+  drawLoginForm();
 }
-
-
